1f916123bc98471cb20bf7e3d26ebe006a3c9525
cdoller <carlosoller1994@gmail.com>
2023-06-15 20:29:31 -0300

    avances en el ejercicio 3

---
+++ Poo_ejercicio3/src/Servicio/OperacionService.java
@@ -0,0 +1,12 @@
+package Servicio;
+
+import Entidad.Operacion;
+import java.util.Scanner;
+// comentario
+
+class OperacionService{
+    public void crearOperacion(int num1, int num2){
+        Operacion op = new Operacion(num1, num2);
+    }
+}
+
