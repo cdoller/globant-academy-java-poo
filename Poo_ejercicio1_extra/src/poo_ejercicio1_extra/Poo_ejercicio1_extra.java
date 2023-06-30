00cfc792d2ff054900eee611ef116d2c9653b040
cdoller <carlosoller1994@gmail.com>
2023-06-29 21:36:58 -0300

    ej 1 extra terminado, simple

---
+++ Poo_ejercicio1_extra/src/poo_ejercicio1_extra/Poo_ejercicio1_extra.java
@@ -0,0 +1,10 @@
+package poo_ejercicio1_extra;
+
+public class Poo_ejercicio1_extra {
+
+    public static void main(String[] args) {
+        Cancion cancion = new Cancion("Out of Time", "The Weeknd");
+        System.out.println(cancion.toString());
+    }
+    
+}
